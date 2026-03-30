ccabd348865071e44c79bfc724c1857eb313d716
Henryk <63795118+henior2@users.noreply.github.com>
2026-03-30 12:43:15 +0200

    add web search toggle command
    Signed-off-by: Henryk <63795118+henior2@users.noreply.github.com>

--- src/bots/telegram/commands/prompt.ts
+++ src/bots/telegram/commands/prompt.ts
@@ -1,3 +1,4 @@
+import { UserRepo } from '@telegram/models/user';
 import { Composer } from 'grammy';
 import type { BotCommand } from 'grammy/types';
 
@@ -6,11 +7,23 @@ export const commands: BotCommand[] = [
   // { command: 'b', description: 'Preffered model B' },
   // { command: 'c', description: 'Preffered model C' },
   // { command: 'set', description: 'Set preferred model a, b or c' },
-  // { command: 'search', description: 'Toggle web search' },
+  { command: 'search', description: 'Toggle web search' },
 ];
 
 export const bot = new Composer();
 
-// TODO implement search toggle command
+bot.command('search', async (ctx) => {
+  if (!ctx.from?.id || !UserRepo) return;
+  const user = UserRepo.get(ctx.from.id);
+  user.preferences.search = !user.preferences.search;
+
+  await ctx.reply(
+    `Web search for models has been turned <b>${user.preferences.search ? 'on' : 'off'}</b>.`,
+    { parse_mode: 'HTML' },
+  );
+
+  UserRepo.set(ctx.from.id, user);
+});
+
 // TODO implement set preferred model command
 // TODO implement prompt commands
